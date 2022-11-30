27c8c62abb475b99f01de2e223f0db4159dd2169
Allan Sampaio <lanbrendo93@gmail.com>
2022-11-29 21:35:06 -0300

    ajustando a responsividade do minha página e criando função editar no botão editar

--- JS/allan.js
+++ JS/allan.js
@@ -1,6 +1,31 @@
+// barras de personalidade//////////////////////////////
 let marker = document.getElementsByClassName('marker')
 
 let array = [90, 75, 80, 75, 85, 50, 60]
 for (i = 0; i < marker.length; i++) {
     marker[i].style.left = `${array[i]}%`
+}
+
+// botão editar////////////////////////////////////////
+let btnEditar = document.getElementById('btnEditar')
+let tituloSessaoDois = document.getElementById('tituloSessaoDois')
+let descricaoSessaoDois = document.getElementById('descricaoSessaoDois')
+
+btnEditar.addEventListener('click', function car() {
+    console.log("entrou");
+    titulo = prompt("Digite o Nome do usuário:")
+    descricao = prompt("Escreva algo que o descreva:")
+    localStorage.setItem('nome', titulo)
+    localStorage.setItem('descricao', descricao)
+
+    let verificador = true
+    window.localStorage.setItem('verificadorLocalStarage', verificador)
+})
+
+if (window.localStorage.getItem('verificadorLocalStarage')) {
+    let nomeLocal = window.localStorage.getItem('nome')
+    let descricaoLocal = window.localStorage.getItem('descricao')
+
+    !nomeLocal ? tituloSessaoDois.innerHTML = "Allan Sampaio" : tituloSessaoDois.innerHTML = nomeLocal
+    !descricaoLocal ? descricaoSessaoDois.innerHTML = "Desenvolvedor .Net junior | C# | Entity Framework | SQL | Testes unitários | WebAPI" :descricaoSessaoDois.innerHTML = descricaoLocal
 }
